d7a69aee262e05427b2cc67802cd72ed5a4b0470
Dylan <dylan@ayedylan.com>
2025-04-07 13:03:47 -0500

    add more input

--- src/input.h
+++ src/input.h
@@ -2,6 +2,16 @@
 
 const char * InputDirection() {
   if (IsKeyDown(KEY_W)) {
-      return "u";
-    }
-}
+    return "u";
+  };
+  if (IsKeyDown(KEY_D)) {
+    return "f";
+  };
+  if (IsKeyDown(KEY_S)) {
+    return "d";
+  }
+};
+
+
+/* There will be a dynamic array that contains each input for each player, and motion inputs will be deduced by the array */
+   
